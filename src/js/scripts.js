45896a92747605f40cc9fdeac6d64dff89c91064
mizza-cz <mizza.cz666@gmail.com>
2024-07-16 18:51:55 +0200

    start

---
+++ src/js/scripts.js
@@ -0,0 +1,6 @@
+//=include ../../node_modules/vanilla-lazyload/dist/lazyload.js
+//=include ../../node_modules/smooth-scroll/dist/smooth-scroll.polyfills.min.js
+document.addEventListener("DOMContentLoaded", function() {
+//=include ../components/**/*.js
+//=include custom/*.js
+});

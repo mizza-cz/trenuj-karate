826acaf93028ff76449ebe5d4eb04c5d1afab2b2
mizza-cz <mizza.cz666@gmail.com>
2024-09-02 12:58:54 +0200

    add fix js-gps

--- src/components/registration/registration.js
+++ src/components/registration/registration.js
@@ -92,18 +92,50 @@ if (fileUpload) {
   };
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  const gpsInputs = document.querySelectorAll(".js-gps");
-
-  gpsInputs.forEach((input) => {
-    // Удаляет нежелательные символы при вводе
-    input.addEventListener("input", function () {
-      this.value = this.value.replace(/[^0-9.]/g, ""); // Удаляет все символы, кроме цифр и точки
-    });
-
-    // Удаляет нежелательные символы при потере фокуса
-    input.addEventListener("blur", function () {
-      this.value = this.value.replace(/[^0-9.]/g, ""); // Удаляет все символы, кроме цифр и точки
+// Restricts input for the given textbox to the given inputFilter.
+function setInputFilter(elements, inputFilter, errMsg) {
+  [
+    "input",
+    "keydown",
+    "keyup",
+    "mousedown",
+    "mouseup",
+    "select",
+    "contextmenu",
+    "drop",
+    "focusout",
+  ].forEach(function (event) {
+    elements.forEach(function (textbox) {
+      textbox.addEventListener(event, function (e) {
+        if (inputFilter(this.value)) {
+          // Accepted value
+          if (["keydown", "mousedown", "focusout"].indexOf(e.type) >= 0) {
+            this.classList.remove("input-error");
+            this.setCustomValidity("");
+          }
+          this.oldValue = this.value;
+          this.oldSelectionStart = this.selectionStart;
+          this.oldSelectionEnd = this.selectionEnd;
+        } else if (this.hasOwnProperty("oldValue")) {
+          // Rejected value - restore the previous one
+          this.classList.add("input-error");
+          this.setCustomValidity(errMsg);
+          this.reportValidity();
+          this.value = this.oldValue;
+          this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+        } else {
+          // Rejected value - nothing to restore
+          this.value = "";
+        }
+      });
     });
   });
-});
+}
+
+setInputFilter(
+  document.querySelectorAll(".js-gps"),
+  function (value) {
+    return /^-?\d*[.,]?\d*$/.test(value);
+  },
+  "Must be a floating (real) number"
+);
